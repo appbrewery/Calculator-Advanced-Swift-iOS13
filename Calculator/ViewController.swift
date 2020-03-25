4f2f97731987c00cf2d6c88d27c0d631a5eea287
Paola Castro <paolacastropaolacastro@gmail.com>
2020-03-25 13:14:06 -0700

    implement non-mathematical functions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,21 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var isFinishedTypingNumber: Bool = false
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot covert an non-numeric string to Double")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
-        //What should happen when a non-number button is pressed
+        isFinishedTypingNumber = true
+        
+        if let symbol = sender.currentTitle{
+            if symbol == "+/-" {
+                displayValue *= -1
+            } else if symbol == "AC" {
+                displayLabel.text = "0"
+            } else if symbol == "%" {
+                displayValue /= 100
+            }
+        }
     
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
-        //What should happen when a number is entered into the keypad
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                print(numValue)
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                if numValue == "." {
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
     
     }
 
