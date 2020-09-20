28280729f2b9afc238a17f2427b4ef1b1feb05d7
Marcus Kim <marcuskim1989@gmail.com>
2020-09-20 15:45:56 -0700

    Refactor to use computed properties with get and set. Refactor to use one guard statement. Fix multiple decimal point bug. Refactor percentage conversion to division by 100

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,9 +12,21 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
+    private var storedValue: String = ""
+    
     private var isFinishedTypingNumber: Bool = true
     
-    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Can't convert display label text to a Double")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
@@ -22,56 +34,46 @@ class ViewController: UIViewController {
         
         isFinishedTypingNumber = true
         
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("Can't convert display label text to a Double")
-        }
-        
         if let calcMethod = sender.currentTitle {
             if calcMethod == "+/-" {
-                displayLabel.text = String(number * -1)
+                displayValue *= -1
             } else if calcMethod == "AC" {
                 displayLabel.text = "0"
+                storedValue = ""
             } else if calcMethod == "%" {
-                displayLabel.text = String(number * 0.01)
+                displayValue = displayValue/100
             }
         }
-    
+        
     }
-
+    
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+        
+        if (sender.currentTitle == "." && storedValue.contains(".")) {
+            return
+        }
+        
+        
         if let numValue = sender.currentTitle {
             
             if isFinishedTypingNumber {
                 displayLabel.text = numValue
+                storedValue.append(numValue)
                 isFinishedTypingNumber = false
             } else {
                 
-                if numValue == "." {
-                    
-                    guard let currentDisplayValue = Double(displayLabel.text!) else {
-                        fatalError("Can't convert display label text to a Double")
-                    }
-                    
-                    let isInt: Bool = floor(currentDisplayValue) == currentDisplayValue
-                    
-                    if !isInt {
-                        return
-                    }
-                    
-                }
-                
-                displayLabel.text = displayLabel.text! + numValue
+                displayLabel.text = storedValue + numValue
+                storedValue.append(numValue)
             }
             
-           
+            
             
         }
         
     }
-
+    
 }
 
